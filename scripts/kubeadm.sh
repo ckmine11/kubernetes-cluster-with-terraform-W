ee5c2afe7683bd09b3aa1adeefdaa62c9f8777f9
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-07-10 22:53:58 +0530

    Update kubeadm.sh

--- scripts/kubeadm.sh
+++ scripts/kubeadm.sh
@@ -1,2 +1,2 @@
 VIP=192.168.5.151
-kubeadm init --config kubeadm-config.yaml --control-plane-endpoint "$VIP:8443"  --upload-certs --pod-network-cidr=10.244.0.0/16
+kubeadm init  --control-plane-endpoint "$VIP:8443"  --upload-certs --pod-network-cidr=10.244.0.0/16
