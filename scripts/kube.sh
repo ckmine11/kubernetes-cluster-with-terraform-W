97b2ea6914a7385ccf4ce2d59079ae6ed6e24493
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-07-10 14:08:02 +0530

    Update kube.sh

--- scripts/kube.sh
+++ scripts/kube.sh
@@ -1,6 +1,6 @@
-KUBE_VERSION=1.24.0
+KUBE_VERSION=1.26.0
 cat <<EOF | sudo tee /etc/resolv.conf 
-nameserver 192.168.5.150
+nameserver 192.168.5.100
 EOF
 
 swapoff -a
@@ -14,7 +14,7 @@ sh -c "echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables"
 sh -c "echo '1' > /proc/sys/net/ipv4/ip_forward"
 
 
-export VERSION=1.23
+export VERSION=1.26
 
 curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/CentOS_8/devel:kubic:libcontainers:stable.repo
 
@@ -30,12 +30,11 @@ systemctl start crio
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.26/rpm/
 enabled=1
 gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-exclude=kubelet kubeadm kubectl
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.26/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
 EOF
 
 dnf install -y kubelet-$KUBE_VERSION -y kubeadm-$KUBE_VERSION  -y kubectl-$KUBE_VERSION --disableexcludes=kubernetes
