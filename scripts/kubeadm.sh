3ce8fe4c33c130b595fa6f8e6ed132ea6801cb2b
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-07-11 15:05:57 +0530

    Update kubeadm.sh

--- scripts/kubeadm.sh
+++ scripts/kubeadm.sh
@@ -1,2 +1,3 @@
-VIP=192.168.5.151
-kubeadm init  --control-plane-endpoint "$VIP:8443"  --upload-certs --pod-network-cidr=10.244.0.0/16
+kubeadm init --control-plane-endpoint 192.168.5.50 --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address=192.168.5.50 --service-cidr 10.0.0.1/16 --upload-certs --cri-socket /var/run/crio/crio.sock
+#VIP=192.168.5.151
+#kubeadm init  --control-plane-endpoint "$VIP:8443"  --upload-certs --pod-network-cidr=10.244.0.0/16
